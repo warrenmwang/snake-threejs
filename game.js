9ea14cb9f7503aedc7d4c28e00f9dcd8f620cb52
Warren Wang <warren.wang2020@gmail.com>
2024-03-22 23:39:35 -0400

    the snake head grows and scene updates as long as user provides movement input. now just need to continue to hammer out the game logic. snake needs to move by itself once the user becomes the prime mover. need to actually show other input to user, like when game is over, snake moving and growing etc.

--- game.js
+++ game.js
@@ -5,6 +5,8 @@
   I can figure that out.
 */
 
+var DEBUG = true;
+
 // assume these are in pixels
 const WIDTH = 500;
 const HEIGHT = 500;
@@ -34,9 +36,13 @@ function euclideanDistance(cell1, cell2){
 export class SnakeGame {
   constructor(gridWidth, gridHeight){
     this.score = 0;
-    this.board = [];
+    this.board = []; // store game state 
+    this.direction = []; // store the directions snake cells should move in
+    this.snakeBody = []; // easy access to snake cells
     this.gridWidth = gridWidth;
     this.gridHeight = gridHeight;
+
+    if (DEBUG) { console.log(`Board size: (W,H) = (${this.gridWidth},${this.gridHeight})`)}
 
     // init board
     this.clearBoard()
@@ -44,8 +50,10 @@ export class SnakeGame {
     // init snake position
     this.head = this.getRandomBoardCell();
     this.board[this.head.row][this.head.col] = 1;
-    this.snakeBody = [this.head];
-    this.tail = this.head;
+    this.snakeBody.push(this.head)
+    this.tail = this.head; // ptr to tail
+
+    if (DEBUG) { console.log(`Head at (x,y) = (${this.head.col},${this.head.row})`)}
 
     // all apples must spawn at least alpha blocks away
     // from the head
@@ -57,14 +65,59 @@ export class SnakeGame {
   // returns true if given cell coordinate does not coincide
   // with any part of the snake's current body (including head
   // and tail), otherwise return false
-  checkCellNotInBody(cell){
+  isCellInBody(cell){
     const row = cell.row;
     const col = cell.col;
     for (const v of this.snakeBody) {
-      if (row === v.row || col === v.col){
-        return false;
+      if (row === v.row && col === v.col){
+        return true;
       }
     }
+    return false;
+  }
+
+  // return true if cell is not in playspace
+  // else return false
+  isCellInWallOrOutOfBounds(cell){
+    // in or jumped beyond any of the walls
+    if(cell.row < 0 || cell.col < 0 || cell.row >= this.gridHeight || cell.col >= this.gridWidth){
+      return true;
+    }
+    return false;
+  }
+
+  // check if player won snake
+  // could check all cells, but that is slower
+  // instead, just check the length of the snake body
+  // if the len of the snake body is equal to the total numbe of
+  // cells in the board, then they have covered the entire board.
+  isWin(){
+    if(this.snakeBody.length == this.gridWidth*this.gridHeight){
+      return true;
+    }
+    return false;
+  }
+
+  // check collision
+  // if given cell coincides with either a snake body or a wall
+  // return true if no collision, else false
+  isNoCollision(cell){
+    // snake body
+    if( this.isCellInBody(cell) ){
+      if (DEBUG) {
+        console.log(`Collision detected: cell in snake body. Snake Body:`);
+        for(const x of this.snakeBody){
+          console.log(`(x,y) = (${x.col},${x.row})`)
+        }
+      }
+      return false;
+    }
+    // a wall
+    if ( this.isCellInWallOrOutOfBounds(cell) ){
+      if (DEBUG) {console.log(`Collision detected: cell in a wall`)}
+      return false;
+    }
+
     return true;
   }
 
@@ -73,26 +126,95 @@ export class SnakeGame {
   // isn't in the snake's body in any way
   genApple(){
     let x = this.getRandomBoardCell();
-    while(!this.checkCellNotInBody(x) && 
+    while(this.isCellInBody(x) && 
           (Math.trunc(euclideanDistance(this.head, x)) >= this.alpha)){
       x = this.getRandomBoardCell();
     }
     this.board[x.row][x.col] = 2;
+    if (DEBUG) { console.log(`Apple spawn: (x,y) = (${x.col}, ${x.row})`)}
   }
 
   // TODO: grow the snake if head coincides with apple cell
-  growSnake(){
+  growSnake(moveDirection, nextCell){
+    /* grows the snake so the tail should just stay in the same spot 
+       and we just update the head forward, thus growing one
+       otherwise, if we didn't grow the snake, the "entire body" moves 
+       forward.
+       therefore, needs to update head, tail, snakebody, directions, board
+
+      Inputs:
+      moveDirection - int value indicating a direction in the cardinal directions
+      nextCell - next cell that the head is to move in
+
+    */
+    if (DEBUG) { console.log('growSnake'); }
+    
+    // update head
+    this.head = nextCell;
+    this.snakeBody.unshift(nextCell)
+
+    // update board making nextCell a snake body part
+    this.board[nextCell.row][nextCell.col] = 1;
+    
     
   }
 
-  // TODO: im not sure how im supposed to handle the movement of the snake
-  // like, how do i know how to make the snake body turn wherever the head had turned ? 
-  updateSnake(){
+  updateSnake(moveDirection){
+    /* movedirection is an int indicating the direction to move in
+      input
+      moveDirection
+        0 - up
+        1 - right
+        2 - down
+        3 - left
+      output
+      0 - ok, updated, continue game.
+      1 - win, game over - no more cells to move in!
+      -1 - loss, game over - collision detected.
+    */
+    if (DEBUG) { console.log('updateSnake'); }
 
+    // want a shallow copy of head
+    let nextCell = { ...this.head };
+    // row is y (up and down) , col is x (left and right)
+    switch(moveDirection){
+      case 0: // up
+        nextCell.row--;
+        break;
+      case 1: // right
+        nextCell.col++;
+        break;
+      case 2: // down
+        nextCell.row++;
+        break;
+      case 3: // left
+        nextCell.col--;
+        break;
+    }
+
+    if (DEBUG) { console.log(`nextCell: (x,y) = (${nextCell.col}, ${nextCell.row})`)}
+    
+    // check if game over -> hit wall or hit self
+    if ( !this.isNoCollision(nextCell) ) {
+      return -1;
+    }
+    // otherwise, update the snake
+    this.growSnake(moveDirection, nextCell);
+    
+    // check win condition (there exists no space left on board)
+    // i believe the easiest way to win is if you try hard and 
+    // only move your snake in a hamiltonian cycle.
+    if (this.isWin()){
+      return 1;
+    }
+
+    // otherwise, continue the game!
+    return 0;
   }
 
-  // clears board
+  // clears/resets board
   clearBoard(){
+    this.board = [];
     for(let i = 0; i < this.gridHeight; i++){
       let tmp = [];
       for(let j = 0; j < this.gridWidth; j++){
@@ -111,6 +233,45 @@ export class SnakeGame {
     let col = Math.trunc(randomValueInRange(0, this.gridWidth));
     return {row:row, col:col};
   }
+
+  // main update entrance function to move from game state n to n+1
+  nextGameState(moveDirection){
+    /* movedirection is an int indicating the direction to move in
+      input
+      moveDirection
+        0 - up
+        1 - right
+        2 - down
+        3 - left
+      output
+      0 - ok, updated, continue game.
+      1 - win, game over - no more cells to move in!
+      -1 - loss, game over - collision detected.
+    */
+
+    if (DEBUG) {
+      console.log(`Before Board:`);
+      printBoard(this.board);
+    }
+   
+    // TODO
+    let updateSnakeResult = this.updateSnake(moveDirection);
+    console.log(`updateSnake returned: ${updateSnakeResult}`)
+
+    if (DEBUG) {
+      console.log(`After Board:`);
+      printBoard(this.board);
+    }
+
+    return this.board;
+  }
+
+}
+
+function printBoard(board) {
+  board.forEach(row => {
+    console.log(row.join(' '));
+  });
 }
 
 // function devTesting(){
@@ -125,3 +286,5 @@ export class SnakeGame {
 // }
 
 // devTesting();
+
+export default SnakeGame;
--- main.js
+++ main.js
@@ -1,44 +1,29 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { SnakeGame } from './game.js';
+import SnakeGame from './game.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
-// SCENE
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x000000 );
-scene.fog = new THREE.Fog( 0x000000, 250, 1400 );
+var DEBUG = true;
 
-// LIGHTS
-// const dirLight = new THREE.DirectionalLight( 0xffffff, 0.4 );
-// dirLight.position.set( 0, 0, 1 ).normalize();
-// scene.add( dirLight );
+// create blocks of different colors that represent our game board.
+// White (0xEDEADE) - Empty Blocks (0)
+// Green (0x00ff00) - Snake (1)
+// Red   (0xFF3131) - Apple (2)
+const WHITE_MESH = new THREE.MeshBasicMaterial( { color : 0xEDEADE } )
+const GREEN_MESH = new THREE.MeshBasicMaterial( { color : 0x00ff00 } )
+const RED_MESH = new THREE.MeshBasicMaterial( { color : 0xFF3131 } )
 
-// const pointLight = new THREE.PointLight( 0xffffff, 4.5, 0, 0 );
-// pointLight.color.setHSL( Math.random(), 1, 0.5 );
-// pointLight.position.set( 0, 100, 90 );
-// scene.add( pointLight );
+let renderer, scene, camera, controls, keyState;
+// use a dict as a mapping of {row,col} -> cube
+// so that later we can use the cell coords to get the cube
+// reference that we may want to alter the color of 
+// in the scene to show updates to our game...
+let cubeDB = new Map();
 
-// define the camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 20;
-// camera.rotation.set(0,0,0);
-
-// define the renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Setup OrbitControls for camera rotation with mouse
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 0, 0); // Set the point to orbit around
-
-// Allow camera movement with WASD, space to go up and shift to go down
-const keyState = {};
-document.addEventListener('keydown', (event) => keyState[event.code] = true);
-document.addEventListener('keyup', (event) => keyState[event.code] = false);
+// time units are in milliseconds
+let lastUpdateTime = performance.now();
+const GAME_TICK = 500;
 
 function moveCamera() {
     let movementSpeed = 0.1;
@@ -72,7 +57,53 @@ function moveCamera() {
     }
 }
 
+// if the user enters a keystroke that is a registered 
+// input for our game, capture it and forward it to the game logic
+function updateGame(game) {
+  /*
+    game - the game object
+  */
 
+  // check to see if it has been at least updateInterval time since the last update
+  let currentTime = performance.now();
+  let timeSinceLastUpdate = currentTime - lastUpdateTime;
+  if (timeSinceLastUpdate < GAME_TICK) {
+    return; // quit if not enough time has passed
+  }
+  // otherwise, update the game (and the lastUpdatetime)!
+  lastUpdateTime = currentTime;
+
+  // movement 
+  var movementKeystroke;
+  if (keyState['ArrowUp']) {
+    movementKeystroke = 0;
+  } else if (keyState['ArrowRight']) {
+    movementKeystroke = 1;
+  } else if (keyState['ArrowDown']) {
+    movementKeystroke = 2;
+  } else if (keyState['ArrowLeft']) {
+    movementKeystroke = 3;
+  } 
+
+  if (movementKeystroke !== undefined ) {
+    console.log(`entered: ${movementKeystroke}`)
+    var newBoard = game.nextGameState(movementKeystroke);
+    if (DEBUG) {
+      console.log("updateGame - received newBoard:")
+      newBoard.forEach(row => {
+        console.log(row.join(' '));
+      });
+    }
+    updateBoard(newBoard);
+    return;
+  }
+
+  // TODO:
+  // pause game
+
+}
+
+// TODO: 
 // Text For Scoreboard. 
 
 // // font loader
@@ -104,67 +135,129 @@ function moveCamera() {
 // const axesHelper = new THREE.AxesHelper( 5 );
 // scene.add( axesHelper );
 
-// create blocks of different colors that represent our game board.
-// White (0xEDEADE) - Empty Blocks (0)
-// Green (0x00ff00) - Snake (1)
-// Red   (0xFF3131) - Apple (2)
 
-// use a dict as a mapping of {row,col} -> cube
-// so that later we can use the cell coords to get the cube
-// reference that we may want to alter the color of 
-// in the scene to show updates to our game...
-let cubeDB = new Map();
 
+
+// Should only be called from the first time board
+// generation.
 function generateCube(row, col, type) {
 
   // select the right color based on type (which is an int)
-  let color = 0xEDEADE;
+  let material = WHITE_MESH;
   if (type === 1) {
-    color = 0x00ff00;
+    material = GREEN_MESH;
   }else if (type === 2) {
-    color = 0xFF3131;
+    material = RED_MESH;
   }
 
   // construct the cube with the right color and location in space
   // z should be a fixed value.
   const cubeLength = 1;
   const geometry = new THREE.BoxGeometry( cubeLength, cubeLength, cubeLength );
-  const material = new THREE.MeshBasicMaterial( { color: color } );
   let cube = new THREE.Mesh( geometry, material );
-  cube.position.set(row*cubeLength, col*cubeLength, 3); // (x,y,z)
+  cube.position.set(col*cubeLength, -row*cubeLength, 3); // (x,y,z)
 
-  // add cube to our hashmap
-  cubeDB[{row:row, col:col}] = cube;
+  return cube;
 
-  // add cube to the scene
-  scene.add( cube );
 }
 
+// Should only be run once
 // generate a bunch of cubes that acts as our game board...
 function generateBoard(board) {
+  let cube;
   for (const row in board){
     for (const col in board[row]) {
-      generateCube(row, col, board[row][col]);
+      // generate a new cube
+      cube = generateCube(row, col, board[row][col]);
+
+      // add cube to our hashmap
+      cubeDB[`${row},${col}`] = cube;
+
+      // add cube to the scene
+      scene.add( cube );
     }
   }
 }
 
-// animate
-function animate(){
-  requestAnimationFrame( animate );
+// refresh the entire board (is this a good idea?)
+// based on the new board passed in (matrix of ints)
+function updateBoard(board) {
+  let newMaterial;
+  let newBoardVal;
+  for (const row in board){
+    for (const col in board[row]) {
+      // get new board value
+      newBoardVal = board[row][col]
+      if (newBoardVal === 0) { newMaterial = WHITE_MESH; }
+      else if (newBoardVal === 1) { newMaterial = GREEN_MESH; }
+      else if (newBoardVal === 2) { newMaterial = RED_MESH; }
+      else { console.error(`Invalid board value encoutered: ${newBoardVal}`)}
+
+      // update the corresponding cell's cube's material (color)
+      cubeDB[`${row},${col}`].material = newMaterial;
+    }
+  }
+}
+
+// animate -- forever loop, runs at refresh rate of screen
+function animate(game){
+  requestAnimationFrame( () => animate(game) );
 
   moveCamera()
+
+  // update game
+  updateGame(game);
 
   renderer.render(scene, camera);
 }
 
-function main() {
-  let game = new SnakeGame(10, 10); // width, height
+function init() {
 
-  // generate the initial board in the scene
+  // SCENE
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x000000 );
+  scene.fog = new THREE.Fog( 0x000000, 250, 1400 );
+
+  // LIGHTS
+  // const dirLight = new THREE.DirectionalLight( 0xffffff, 0.4 );
+  // dirLight.position.set( 0, 0, 1 ).normalize();
+  // scene.add( dirLight );
+
+  // const pointLight = new THREE.PointLight( 0xffffff, 4.5, 0, 0 );
+  // pointLight.color.setHSL( Math.random(), 1, 0.5 );
+  // pointLight.position.set( 0, 100, 90 );
+  // scene.add( pointLight );
+
+  // define the camera
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = 20;
+  // camera.rotation.set(0,0,0);
+
+  // define the renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // Setup OrbitControls for camera rotation with mouse
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0, 0); // Set the point to orbit around
+
+  // Allow camera movement with WASD, space to go up and shift to go down
+  keyState = {};
+  document.addEventListener('keydown', (event) => keyState[event.code] = true);
+  document.addEventListener('keyup', (event) => keyState[event.code] = false);
+
+  // create game object
+  var game = new SnakeGame(10, 10); // width, height
+
+
+  // draw the initial board in the scene from the inited board
   generateBoard(game.board);
 
-  animate();
+  // run the while loop...
+  animate(game);
 }
 
-main();
+init()
